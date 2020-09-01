e20ce86627aa5f23078959c45567bc03e4ca3c8d
pmsdOliveira <46815374+pmsdOliveira@users.noreply.github.com>
2020-09-01 15:27:41 +0100

    MikroORM Setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import mikroConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+  orm.getMigrator().up();
+};
+
+main().catch((err) => {
+  console.error(err);
+});
