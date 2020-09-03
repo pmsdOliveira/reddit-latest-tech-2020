eb1448acd1a1f5872f2a4e1c632b7f488ff15c9e
pmsdOliveira <46815374+pmsdOliveira@users.noreply.github.com>
2020-09-03 10:10:56 +0100

    Register Resolver

--- dist/mikro-orm.config.js
+++ dist/mikro-orm.config.js
@@ -6,11 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const constants_1 = require("./constants");
 const Post_1 = require("./entities/Post");
 const path_1 = __importDefault(require("path"));
+const User_1 = require("./entities/User");
 exports.default = {
     dbName: "lireddit",
     type: "postgresql",
     debug: !constants_1.__prod__,
-    entities: [Post_1.Post],
+    entities: [Post_1.Post, User_1.User],
     migrations: {
         path: path_1.default.join(__dirname, "./migrations"),
         pattern: /^[\w-]+\d+\.[tj]s$/,
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,12 +2,13 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import { User } from "./entities/User";
 
 export default {
   dbName: "lireddit",
   type: "postgresql",
   debug: !__prod__,
-  entities: [Post],
+  entities: [Post, User],
   migrations: {
     path: path.join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
